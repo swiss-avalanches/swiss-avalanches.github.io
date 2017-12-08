0664d08461902d6633a938060b5c7e052f85bb1f
Jean-Baptiste Cordonnier <jbcdnr@gmail.com>
2017-12-08 18:10:29 +0100

    Update polar selection

--- website/static/js/polar.js
+++ website/static/js/polar.js
@@ -2,63 +2,138 @@
 // should add overlay
 // how do we do selection ?
 
-function createPolar() {
-  var maxAltitude = 4500;
-  var minAltitude = 1000;
+var propertiesPolar = {}
 
-  var width = 960,
-    height = 500,
-    radius = Math.min(width, height) / 2 - 30;
+function createPolar(accidentsData, addFilter) {
+  propertiesPolar.maxAltitude = 4500;
+  propertiesPolar.minAltitude = 1000;
+  propertiesPolar.width = 960
+  propertiesPolar.height = 500,
+  propertiesPolar.radius = Math.min(propertiesPolar.width, propertiesPolar.height) / 2 - 30;
 
-  var r = d3.scaleLinear()
-    .domain([maxAltitude, minAltitude])
-    .range([0, radius]);
+  propertiesPolar.r = d3.scaleLinear()
+    .domain([propertiesPolar.maxAltitude, propertiesPolar.minAltitude])
+    .range([0, propertiesPolar.radius]);
 
-  var line = d3.radialLine()
-    .radius(function(d) {
-      return r(d[1]);
-    })
-    .angle(function(d) {
-      return d[0] - Math.PI ;
-    });
+  function angleFromCoord(x, y) {
+    // cartesian not centered but with x right direction, y top direction
+    // return theta in [0, 2pi] on trigonometric circle (start on the right and goes anti clock wise)
+    var theta = Math.atan(Math.abs(y / x));
+    if (x >= 0 && y >= 0) {  // cadran 1
+      theta = theta
+    } else if (x <= 0 && y >= 0) {  // cadran 2
+      theta = Math.PI - theta
+    } else if (x <= 0 && y <= 0) {  // cadran 3
+      theta = Math.PI + theta;
+    } else if (x >= 0 && y <= 0) {  // cadran 4
+      theta = 2 * Math.PI - theta
+    }
 
-
-  // function mousemove(d, i) {
-  //   console.log(d3.mouse(this));
-  // }
-
-  var drag = d3.drag()
-    .subject(function(d) { return d == null ? {x: d3.event.x, y: d3.event.y} : d; })
-    .on("start", dragged);
-
-  function dragged(d) {
-    console.log(d3.event.x);
-    d[0] = d3.event.x, d[1] = d3.event.y;
-    // if (this.nextSibling) this.parentNode.appendChild(this);
-    // d3.select(this).attr("transform", "translate(" + d + ")");
+    return theta
   }
 
-  var svg = d3.select("#polar").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .append("g")
-    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
-    .call(drag);
-    
+  var dragStartAngle = undefined,
+    dragEndAngle = undefined;
 
-  var gr = svg.append("g")
+  var drag = d3.drag()
+    .on("drag", dragged)
+    .on("start", startDrag)
+    .on("end", endDrag)
+
+  function startDrag() {
+    var x = d3.event.x,
+    y = - d3.event.y;
+    var angle = (Math.PI - angleFromCoord(x, y)) - Math.PI / 2;
+    dragStartAngle = angle;
+    dragEndAngle = angle
+
+    if (d3.selectAll(".selection path").size() == 0) {
+      d3.selectAll(".selection").append("path")
+        .attr("opacity", 0.5)
+        .attr("fill", "#3498db")
+        .on("click", function(){
+          propertiesPolar.svg.selectAll(".selection").selectAll("*").remove();
+        });
+    }
+
+    d3.selectAll(".selection path")
+      .attr("d", arc([0, propertiesPolar.radius, dragStartAngle, dragEndAngle]));
+  }
+
+  function dragged(d) {
+    var x = d3.event.x,
+    y = - d3.event.y;
+    var angle = (Math.PI - angleFromCoord(x, y)) - Math.PI / 2;
+    var maxDelta = Math.PI + 0.1
+
+    if (Math.abs(angle - dragEndAngle) < maxDelta) {
+      dragEndAngle = angle;
+    } else if (Math.abs(2 * Math.PI + angle - dragEndAngle) < maxDelta) {
+      dragEndAngle = 2 * Math.PI + angle;
+    } else if (Math.abs(-2 * Math.PI + angle - dragEndAngle) < maxDelta) {
+      dragEndAngle = -2 * Math.PI + angle;
+    }
+
+    d3.selectAll(".selection path")
+      .attr("d", arc([0, propertiesPolar.radius, dragStartAngle, dragEndAngle]));
+  }
+
+  function endDrag() {
+    // TODO call addFilter
+    // TODO disable if no aspect selected
+    console.log(dragStartAngle, dragEndAngle, )
+    var selectedAspects = aspectRangeAngle(dragStartAngle, dragEndAngle)
+    if (selectedAspects.length == 0) {
+      // reject
+    } if (selectedAspects.length == 1) {
+      filterName = "Aspect {}".format(selectedAspects[0])
+    } else {
+      filterName = "Aspect {}-{}".format(selectedAspects[0], selectedAspects[selectedAspects.length - 1])
+    }
+    var filterFunction = function(d) {
+      return selectedAspects.includes(d['Aspect'])
+    }
+    console.log(filterName)
+    
+    
+    dragStartAngle = undefined;
+    dragEndAngle = undefined;
+  }
+
+  propertiesPolar.svg = d3.select("#polar").append("svg")
+    .attr("width", propertiesPolar.width)
+    .attr("height", propertiesPolar.height)
+    .append("g")
+    .attr("transform", "translate(" + propertiesPolar.width / 2 + "," + propertiesPolar.height / 2 + ")")
+  
+
+  var arc = d3.arc()
+    .innerRadius(function (d) { return d[0] })
+    .outerRadius(function (d) { return d[1] })
+    .startAngle(function (d) { return d[2] })
+    .endAngle(function (d) { return d[3] });
+
+  // full clickable cirle
+  propertiesPolar.svg.append("g")
+  .attr("class", "clickable")
+  .append("path")
+    .attr("d", arc([0, propertiesPolar.radius, 0, 2 * Math.PI]))
+    .attr("fill", "white")
+    .call(drag);
+
+  var gr = propertiesPolar.svg.append("g")
     .attr("class", "r axis")
     .selectAll("g")
-    .data(r.ticks(4).slice(1))
+    .data(propertiesPolar.r.ticks(4).slice(1))
     .enter().append("g");
 
   gr.append("circle")
-    .attr("r", r)
+    .attr("r", propertiesPolar.r)
     .attr("fill", "none")
     .attr("stroke", "grey")
     .attr("stroke-width", "1px");
 
-  var ga = svg.append("g")
+  var ga = propertiesPolar.svg.append("g")
     .attr("class", "a axis")
     .selectAll("g")
     .data(d3.range(0, 360, 30))
@@ -68,54 +143,52 @@ function createPolar() {
     });
 
   ga.append("line")
-    .attr("x2", radius);
+    .attr("x2", propertiesPolar.radius);
 
-  var arc = d3.arc()
-    .innerRadius(function (d) { console.log(d, r(maxAltitude)); return d[0] })
-    .outerRadius(function (d) { return d[1] })
-    .startAngle(function (d) { return d[2] })
-    .endAngle(function (d) { return d[3] });
-
-  svg.append("g")
+  propertiesPolar.svg.append("g")
     .attr("class", "selection")
-    .selectAll("g")
-    .data([[0, r(minAltitude), - Math.PI / 2, 2 * Math.PI / 3]])
-    .enter().append("path")
-      .attr("d", arc)
-      .attr("fill", "#3498db")
-      .attr("opacity", 0.5)
 
-  
-  var color = d3.scaleOrdinal(d3.schemeCategory20);
+  updatePolar(accidentsData, addFilter)
+}
 
+
+function updatePolar(data, addFilter) {
   var line = d3.radialLine()
     .radius(function(d) {
-      return r(d[1]);
+      return propertiesPolar.r(d[1]);
     })
     .angle(function(d) {
       return d[0];
     });
 
-  svg.selectAll("point")
-    .data(accidentsData)
-    .enter()
-    .append("circle")
-    .attr("class", "point")
-    .attr("transform", function(d) {
-      var angle = aspect(d['Aspect'], 'angle');
-      var elevation = d['Elevation'];
-      var coors = line([[angle, elevation]]).slice(1).slice(0, -1);
-      return "translate(" + coors + ")"
-    })
-    .attr("r", function(d) {
-      var killed = d['killed']
-      return 3+4*killed
-    })
-    .attr("stroke", "white")
-    .attr("fill", function(d){
-      return dangerColor(d['Danger level']);
-    })
-    .on('click', function (d) {
-      console.log(d);
+  var points = propertiesPolar.svg.selectAll(".point")
+    .data(data, function(d) {
+      console.log(d.Date + d.Latitude + d.Longitude)
+      return d.Date + ", " + d.Latitude + ", " + d.Longitude;
     });
+
+  var pointsEnter = points.enter()
+  
+  pointsEnter.append("circle")
+      .attr("class", "point")
+      .attr("stroke", "white")
+      .on('click', function (d) {
+        console.log(d);
+      })
+      .attr("transform", function(d) {
+        var angle = aspect(d['Aspect'], 'angle');
+        var elevation = d['Elevation'];
+        var coors = line([[angle, elevation]]).slice(1).slice(0, -1);
+        return "translate(" + coors + ")"
+      })
+      .attr("r", function(d) {
+        var killed = d['killed']
+        return 3+4*killed
+      })
+      .attr("fill", function(d){
+        return dangerColor(d['Danger level']);
+      })
+  
+  points.exit().remove();
+    
 }

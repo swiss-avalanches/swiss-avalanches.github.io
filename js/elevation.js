9a4645533516f56b15dd81094067118e340bbdef
Brune Bastide <brune.bastide@epfl.ch>
2017-12-10 23:11:18 +0100

    elevation where everybody dies

--- js/elevation.js
+++ js/elevation.js
@@ -1,32 +1,6 @@
 var propertiesElevation = {}
 var elevations = [0, 500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500]; //TODO linspace si existe?
 
-// to be mapped with elevations array
-function bucketifyElevation(data){
-    var buckets = d3.range(9).map(function(d){return 0;});
-    for (let i = 0; i < data.length; i++) {
-        var element = data[i];
-        var elementElevation = element.altitude - (element.altitude % 500) + 500;
-        buckets[elevations.indexOf(elementElevation)] +=  element.killed; 
-    }
-    return buckets;
-}
-
-// naive approach: devrait retourner array avec plusieurs fois les memes niveaux 
-function cumulElevation(data){
-    var new_data = [];
-    var j=0;
-    for (let i = 0; i < data.length; i++) {
-        var element = data[i];
-        var numKilled = element.killed;
-        for (let k = 0; k < numKilled; k++){
-            new_data[j+k] = element.altitude - (element.altitude % 500) + 500;
-        }
-        j += numKilled;
-    }
-    return new_data;
-}
-
 function devel(data) {
     function filter(x) {
         var newElev = x.Elevation - (x.Elevation % 500) + 500;
@@ -43,61 +17,69 @@ function devel(data) {
         return _.reduce(array, redFunc, 0);
     }
 
+
     var groupped = _.mapValues(_.groupBy(_.map(data, filter), 'elevation'), filter2);
     return groupped;
 }
 
 function createElevation(accidentsData, addFilter, removeFilter) {
-     const histData = devel(accidentsData);
+    var histData = devel(accidentsData);
 
-     var svg = d3.select("#elevation").append("svg")
-    /*  var data = cumulElevation(accidentsData); 
-    //var data =  [500, 500, 1000, 1500, 500, 2000, 4500, 2500, 2500, 2500, 3000, 3500, 1500]
+    var elevations = Object.keys(histData);
+    var kills = Object.values(histData);
 
-    var formatCount = d3.format(",.0f");
-    
-    var svg = d3.select("#elevation").append("svg"),
-        margin = {top: 10, right: 30, bottom: 30, left: 30},
-        width = +svg.attr("width") - margin.left - margin.right,
-        height = +svg.attr("height") - margin.top - margin.bottom,
-        g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-    
-    var x = d3.scaleLinear()
-            .domain([500, 5000])
-            .rangeRound([0, width]);
-    
-    var bins = d3.histogram()
-        .domain(x.domain())
-        .thresholds(x.ticks(10))
-        (data);
-    
+    var newData = [];
+
+    for(var i = 0; i < elevations.length; ++i) {
+         newData.push({
+            elevation: elevations[i],
+            killed: kills[i]
+        });
+    }
+
+    console.log(newData);
+
+    var margin = {top: 20, right: 20, bottom: 30, left: 40},
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+
+    var x = d3.scaleBand()
+            .range([0, width])
+            .padding(0.1);
+
     var y = d3.scaleLinear()
-        .domain([0, d3.max(bins, function(d) { return d.length; })])
-        .range([height, 0]);
-    
-    var bar = g.selectAll(".bar")
-      .data(bins)
-      .enter().append("g")
+            .range([height, 0]);
+
+    var svg = d3.select("body").append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", 
+                  "translate(" + margin.left + "," + margin.top + ")");
+
+    x.domain(data.map(function(d) { return d.elevation; }));
+    y.domain([0, d3.max(data, function(d) { return d.killed; })]);
+
+
+    // append the rectangles for the bar chart
+    svg.selectAll(".bar")
+        .data(newData)
+        .enter().append("rect")
         .attr("class", "bar")
-        .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; });
-    
-    bar.append("rect")
-        .attr("x", 1)
-        .attr("width", x(bins[0].x1) - x(bins[0].x0)-1) // retiré
-        .attr("height", function(d) { return height - y(d.length); });
-    
-    bar.append("text")
-        .attr("dy", ".75em")
-        .attr("y", 6)
-        .attr("x", (x(bins[0].x1) - x(bins[0].x0)) / 2)
-        .attr("text-anchor", "middle")
-        .text(function(d) { return formatCount(d.length); });
-    
-    g.append("g")
-        .attr("class", "axis axis--x")
+        .attr("x", function(d) { return x(d.elevation); })
+        .attr("width", x.bandwidth())
+        .attr("y", function(d) { return y(d.killed); })
+        .attr("height", function(d) { return height - y(d.killed); });
+
+    // add the x Axis
+     svg.append("g")
         .attr("transform", "translate(0," + height + ")")
         .call(d3.axisBottom(x));
-     */
+
+    // add the y Axis
+    svg.append("g")
+        .call(d3.axisLeft(y));
+    
     updateElevation(data, addFilter, removeFilter) 
 }
 

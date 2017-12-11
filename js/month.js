47ace9dbe0ea547041fe3aee2464a559424212c5
Brune Bastide <brune.bastide@epfl.ch>
2017-12-11 10:29:02 +0100

    working on it

---
+++ js/month.js
@@ -0,0 +1,34 @@
+// We will compute in this histogram the number of accidents per month
+
+function removeUndef(data) {
+    return _.omit(data, _.filter(_.keys(data), function(key) { return _.isUndefined(data[key]) }))
+}
+
+function groupByMonth(data) {
+
+    function filterDate(x) {
+        var date = x.Date;//(x.Date).substring(3, 4);;
+        return {
+            Date: date
+        };
+    }
+
+    var dataDate = removeUndef(_.map(data, filterDate));
+    
+    function filterMonth(x) {
+        var month = x
+    }
+
+
+    return ;
+}
+
+function createMonth(accidentsData, addFilter, removeFilter) {
+    //console.log(groupByMonth(accidentsData))
+    if (!){} 
+    console.log(accidentsData[0].Date)
+}
+
+function updateMonth(accidentsData, addFilter, removeFilter) {
+    
+} 
